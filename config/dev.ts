59c4c5d5fd8e348e90938eeb047965f2122f7368
themusecatcher <themusecatcher@163.com>
2025-01-08 16:19:54 +0800

    update

--- config/dev.ts
+++ config/dev.ts
@@ -6,8 +6,22 @@ export default {
   },
   env: { // 设置环境变量
     NODE_ENV: '"development"', // JSON.stringify('development')
-    TARO_APP_PROXY: '"https://request.cn"'
+    TARO_APP_PROXY: '"https://www.request.cn"'
   },
   mini: {},
-  h5: {}
+  h5: {
+    devServer: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'https://www.h5.cn',  // h5 服务端地址
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          }
+        }
+      }
+    }
+  }
 } satisfies UserConfigExport
